dc7c5993192fc3b9634eba499ac6be34b51cc7d9
Patrick Heflin <patrickslx@yahoo.com>
2021-04-07 10:26:50 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+screen.lockOrientation('landscape');
+
+const keys = {
+  'a': 'C',
+  'w': 'C#',
+  's': 'D',
+  'e': 'D#',
+  'd': 'E',
+  'f': 'F',
+  't': 'F#',
+  'g': 'G',
+  'y': 'G#',
+  'h': 'A',
+  'u': 'A#',
+  'j': 'B',
+  'k': 'C'
+}
+
+var currentKey = ''
+let octave = 4
+
+window.addEventListener('keydown', (event) => {
+  if (event.key !== undefined) {
+    currentKey = event.key
+  }
+})
+
+function playNote() {
+  const synth = new Tone.Synth().toDestination();
+  const now = Tone.now()
+
+  synth.triggerAttack(`${keys[currentKey]}${octave}`, now);
+  synth.triggerRelease(now + 1)
+}
+
+function upOctave() {
+  octave += 1
+}
+
+function downOctave() {
+  octave -= 1
+}
