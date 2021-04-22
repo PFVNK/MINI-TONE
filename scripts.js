9c62ed4a14267e12579c7c1beef6ec03bba9beb8
Patrick Heflin <patrickslx@yahoo.com>
2021-04-22 14:38:41 -0500

    add ability to change synth type

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-screen.lockOrientation("landscape-primary");
-
 const keys = {
   'a': 'C',
   'w': 'C#',
@@ -16,27 +14,130 @@ const keys = {
   'k': 'C'
 }
 
+var synth
+var now
+
 var currentKey = ''
 let octave = 4
+let clicked = false
+
+let k = document.getElementsByClassName('key')
+
+let instruments = ['Synth', 'FMSynth', 'AMSynth', 'MetalSynth']
+let currentSynth = 'Synth'
+
+
+function setUp() {
+  synth = new Tone.Synth().toDestination();
+  now = Tone.now()
+}
 
 window.addEventListener('keydown', (event) => {
+  if (event.repeat) { return }
   if (event.key !== undefined) {
-    currentKey = event.key
+    if (event.key === 'z') {
+      downOctave()
+    }
+
+    if (event.key === 'x') {
+      upOctave()
+    }
+
+    if (event.key in keys) {
+      currentKey = event.key
+    } else {
+      return
+    }
   }
+
+  playNote()
+})
+
+window.addEventListener('keyup', (event) => {
+  if (event.repeat) { return }
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  stopNote()
 })
 
 function playNote() {
-  const synth = new Tone.Synth().toDestination();
-  const now = Tone.now()
+  synth.triggerAttack(`${keys[currentKey]}${octave}`);
+}
 
-  synth.triggerAttack(`${keys[currentKey]}${octave}`, now);
-  synth.triggerRelease(now + 1)
+function stopNote() {
+  synth.triggerRelease()
+}
+
+function nextSynth() {
+  currentSynth = instruments[instruments.indexOf(currentSynth) + 1]
+
+  if (currentSynth === 'Synth') {
+    synth = new Tone.Synth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'Synth'
+  }
+
+  if (currentSynth === 'FMSynth') {
+    synth = new Tone.FMSynth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'FMSynth'
+  }
+
+  if (currentSynth === 'AMSynth') {
+    synth = new Tone.AMSynth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'AMSynth'
+  }
+
+  if (currentSynth === 'MetalSynth') {
+    synth = new Tone.MetalSynth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'MetalSynth'
+  }
+}
+
+function previousSynth() {
+  currentSynth = instruments[instruments.indexOf(currentSynth) - 1]
+
+  if (currentSynth === 'Synth') {
+    synth = new Tone.Synth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'Synth'
+  }
+
+  if (currentSynth === 'FMSynth') {
+    synth = new Tone.FMSynth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'FMSynth'
+  }
+
+  if (currentSynth === 'AMSynth') {
+    synth = new Tone.AMSynth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'AMSynth'
+  }
+
+  if (currentSynth === 'MetalSynth') {
+    synth = new Tone.MetalSynth().toDestination();
+    document.getElementsByClassName('synth-name')[0].innerHTML = 'MetalSynth'
+  }
+}
+
+function addDelay() {
+  if (clicked) {
+    clicked = false
+    console.log(synth)
+  } else if (!clicked) {
+    var feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
+    synth.connect(feedbackDelay)
+    clicked = true
+    console.log(feedbackDelay)
+  }
+
 }
 
 function upOctave() {
   octave += 1
+  document.getElementsByClassName('oct-num')[0].innerHTML = octave
+  console.log(octave)
 }
 
 function downOctave() {
   octave -= 1
+  document.getElementsByClassName('oct-num')[0].innerHTML = octave
+  console.log(octave)
 }
